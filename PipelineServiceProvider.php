5db57c656721aac462cc9593386a01a3ee1a891a
satupersen <alfiyanmail24@gmail.com>
2020-02-29 17:58:16 +0700

    first commit

---
+++ PipelineServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Satupersen\Pipeline;
+
+use Satupersen\Contracts\Pipeline\Hub as PipelineHubContract;
+use Satupersen\Contracts\Support\DeferrableProvider;
+use Satupersen\Support\ServiceProvider;
+
+class PipelineServiceProvider extends ServiceProvider implements DeferrableProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(
+            PipelineHubContract::class, Hub::class
+        );
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [
+            PipelineHubContract::class,
+        ];
+    }
+}
